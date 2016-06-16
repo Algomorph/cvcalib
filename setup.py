e0f0cb7fa1f05d66e2723046c36c642cdcc97a02
Gregory Kramida <algomorph@gmail.com>
2016-06-16 16:24:25 -0400

    Setup added, Pose constructor improved & __str__ added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cvcalib',
+    version='0.3.1',
+    packages=['calib', 'common', 'audiosync', 'multistereo'],
+    url='',
+    license='Apache v2',
+    author='Gregory Kramida',
+    author_email='algomorph@gmail.com',
+    description=''
+)
